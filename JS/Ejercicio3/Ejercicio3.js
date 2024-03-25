c216e1e921a7fa7ae212acb4e5ed3855be8e15db
Pau Alvarez <alvarez.paula.maria@gmail.com>
2024-03-25 19:50:16 -0300

    Comentario agregado en el Ejercicio 3

--- JS/Ejercicio3/Ejercicio3.js
+++ JS/Ejercicio3/Ejercicio3.js
@@ -10,3 +10,8 @@ document.getElementById('inputTexto').addEventListener('input', function () {
         document.getElementById('spanResultado').style.color = 'red';
     }
 });
+
+
+// addEventListener es un método en JavaScript que permite adjuntar un evento a un elemento HTML, de manera que cuando ocurra ese evento en el elemento, 
+// se ejecute una función específica. En este caso, el evento que estamos escuchando es 'input', 
+// que se activa cada vez que el valor del elemento cambia, como cuando el usuario ingresa texto en un cuadro de texto.
